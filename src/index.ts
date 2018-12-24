79310874bf6d457a925cee2fc680829d86779045
Andrei Neacșu <amneacsu@gmail.com>
2018-12-24 08:40:31 +0200

    :wqwip

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 import Comm from './comm';
-import { listen } from './sensor';
+import { tone } from './speaker';
+import { outputStepSpeed, MOTOR_A, MOTOR_D } from './motor';
 
 const port = new Comm('/dev/tty.EV3-SerialPort-1');
 
 setInterval(() => {
-  const act = listen();
-
-  port.dispatch([act], (data) => {
-    console.log(data);
-  }, (h) => {
-    // one byte reserve global alloc
-    h.writeUInt16LE(4, 5);
-  });
-}, 1000);
+  const t = tone(1, 200, 50);
+  const x = outputStepSpeed(MOTOR_A, 50, 0, 200, 100, true);
+  const y = outputStepSpeed(MOTOR_D, 50, 0, 200, 100, true);
+  port.dispatch([t, x, y]);
+}, 500);
--- src/motor.ts
+++ src/motor.ts
@@ -1,16 +1,50 @@
 import Message from './message';
 
-export const outputStepSpeed = () => {
+export const MOTOR_A = 0x01;
+export const MOTOR_B = 0x02;
+export const MOTOR_C = 0x04;
+export const MOTOR_D = 0x08;
+
+export const outputStepSpeed = (
+  motors: number,
+  power: number,
+  step1: number,
+  step2: number,
+  step3: number,
+  brake: boolean = true,
+) => {
   const message = new Message();
 
   message.lc0(0xAE); // opOUTPUT_STEP_SPEED
   message.lc0(0); // LAYER = 0
-  message.lc0(0x03); // NOS = MOTOR_A + MOTOR_B
-  message.lc1(50); // SPEED = 50 – Power level, [-100 - 100]
-  message.lc0(0); // STEP1
-  message.lc2(900); // STEP2
-  message.lc2(180); // STEP3
-  message.lc0(1); // BRAKE
+  message.lc0(motors);
+  message.lc1(power);
+  message.lc0(step1);
+  message.lc2(step2);
+  message.lc2(step3);
+  message.lc0(Number(brake));
+
+  return message;
+}
+
+export const outputTimeSpeed = (
+  motors: number = 100,
+  power: number,
+  step1: number,
+  step2: number,
+  step3: number,
+  brake: boolean = true,
+) => {
+  const message = new Message();
+
+  message.lc0(0xAF); // opOUTPUT_TIME_SPEED
+  message.lc0(0); // LAYER = 0
+  message.lc0(motors);
+  message.lc1(power);
+  message.lc0(step1);
+  message.lc2(step2);
+  message.lc2(step3);
+  message.lc0(Number(brake));
 
   return message;
 }
