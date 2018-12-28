647e6adfc5dd78901d09f905894026b39aef9753
Andrei Neacșu <amneacsu@gmail.com>
2018-12-29 01:57:59 +0200

    no ts

---
+++ sample/index.js
@@ -0,0 +1,22 @@
+const EV3 = require('../lib');
+
+const brick = new EV3.Brick();
+
+const port = process.argv[2] || '/dev/tty.EV3-SerialPort-1';
+
+brick.connect(port).then(() => {
+  const motor = new EV3.Input(EV3.Input.MOTOR_A);
+  const speaker = new EV3.Speaker();
+  const screen = new EV3.Screen();
+
+  brick.dispatch([
+    motor.setPower(100),
+  ]);
+
+  setInterval(() => {
+    brick.dispatch([
+      speaker.tone(1, 100, 50),
+      screen.fillRect(0, 0, 0, 100, 100),
+    ]);
+  }, 1000);
+});
