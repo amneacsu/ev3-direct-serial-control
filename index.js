842153e0ad43756e74a8d0f5133195e4c1e3f9cc
Andrei Neacșu <amneacsu@gmail.com>
2019-01-02 15:57:26 +0200

    refactor 2

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const port = process.argv[2] || '/dev/tty.EV3-SerialPort';
 const brick = new EV3.Brick();
 
 brick.connect(port).then(() => {
-  const motor = new EV3.Output(EV3.Output.MOTOR_A);
+  const motor = new EV3.Output(brick, EV3.Output.MOTOR_A);
   const speaker = new EV3.Speaker(brick);
-  const screen = new EV3.Screen();
-  const sensor = new EV3.Input();
+  const screen = new EV3.Screen(brick);
+  const sensor = new EV3.Input(brick);
 
   const run = () => {
     speaker.tone(1, 100, 50).then(() => {
