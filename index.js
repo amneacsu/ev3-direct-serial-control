7bc7d157b33f2ec56f1166a3714a3f1656c8736b
Andrei Neacșu <amneacsu@gmail.com>
2019-01-02 15:38:17 +0200

    refactor

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const EV3 = require('./lib');
-const { command, LCX, LVX, GVX } = require('./lib/commands');
-
-const brick = new EV3.Brick();
 
 const port = process.argv[2] || '/dev/tty.EV3-SerialPort';
 
+const brick = new EV3.Brick();
+
 brick.connect(port).then(() => {
-  // const motor = new EV3.Output(EV3.Output.MOTOR_A);
-  const speaker = new EV3.Speaker();
-  // const screen = new EV3.Screen();
-  // const sensor = new EV3.Input();
+  const motor = new EV3.Output(EV3.Output.MOTOR_A);
+  const speaker = new EV3.Speaker(brick);
+  const screen = new EV3.Screen();
+  const sensor = new EV3.Input();
 
   const run = () => {
-    brick.dispatch([
-      speaker.tone(1, 100, 50),
-    ]).then(resp => {
-      console.log(resp);
+    speaker.tone(1, 100, 50).then(() => {
       brick.disconnect();
       process.exit();
     });
   };
 
-  // setInterval(run, 1000);
   run();
 });
